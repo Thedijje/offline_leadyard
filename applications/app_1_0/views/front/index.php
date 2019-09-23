5fb829162a7eabf8ac9327638fb9e3c9020fc881
dheeraj <dheeraj@cteinternational.net>
2019-09-23 14:19:09 +0530

    updated boilerplate to add custome home page with some instruction

--- applications/app_1_0/views/front/index.php
+++ applications/app_1_0/views/front/index.php
@@ -8,10 +8,11 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Bare - Start Bootstrap Template</title>
+  <title>Codeigniter boilerplate - @thedijje</title>
 
   <!-- Bootstrap core CSS -->
   <link href="<?php echo base_url('static/front/css/bootstrap.min.css')?>" rel="stylesheet">
+  <link href="<?php echo base_url('static/front/css/fontawesome.min.css')?>" rel="stylesheet">
 
 </head>
 
@@ -20,25 +21,23 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Start Bootstrap</a>
+      <a class="navbar-brand" href="#">CI Boilerplate</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#">Home
+            <a class="nav-link" href="<?php echo base_url()?>">Home
               <span class="sr-only">(current)</span>
             </a>
           </li>
+          
           <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
+            <a class="nav-link" href="https://github.com/thedijje/ci_boilerplate">Learn more</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" href="https://twitter.com/@thedijje">Contact</a>
           </li>
         </ul>
       </div>
@@ -49,12 +48,23 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-12 text-center">
-        <h1 class="mt-5">A Bootstrap 4 Starter Template</h1>
-        <p class="lead">Complete with pre-defined file paths and responsive navigation!</p>
-        <ul class="list-unstyled">
-          <li>Bootstrap 4.3.1</li>
-          <li>jQuery 3.4.1</li>
-        </ul>
+        <h1 class="mt-5">Codeigniter 3</h1>
+        <p class="lead">Learn more about boilerplate, installation and setuo!</p>
+        <p>
+          <a href="https://github.com/thedijje/ci_boilerplate" target="_blank"><button class="btn btn-primary">View on github</button></a>
+        </p>
+        
+        <div class="text-center">
+
+          <ul class="list-unstyled">
+            <li>Edit default route from <code>Home</code></li>
+            <li>Copy <code>.ENV_example</code> to <code>.ENV</code> and update value as required</li>
+            <li>Application directory location at <code>applications/app_1_0</code>, you can change so in <code>.ENV</code> file</li>
+          </ul>
+          
+
+
+        </div>
       </div>
     </div>
   </div>
