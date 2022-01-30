96dcd03c52c310d3cf47b4f7c8a9919cd50b6d8c
Dheeraj jha <dheeraj@thedijje.com>
2022-01-30 15:06:15 +0530

    updated size of qr code

--- app/1.0/libraries/Qr.php
+++ app/1.0/libraries/Qr.php
@@ -6,7 +6,7 @@ class Qr
     public function generate($text='code data text')
     {
         $filename = $this->qr_path.time().'_filename.png';
-        QRcode::png($text, $filename,QR_ECLEVEL_L,4);
+        QRcode::png($text, $filename,QR_ECLEVEL_H);
 
         echo "<img src='".base_url($filename)."'>";
     }
